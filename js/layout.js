a78eda3707b83c57e6ddaffa637c37373a28f98e
Nicolas Jandre <nicolasjandre@live.com>
2022-12-05 20:38:22 -0300

    localstorage is now working on layout  theme color

--- js/layout.js
+++ js/layout.js
@@ -2,9 +2,21 @@ export class Layout {
     constructor() {
         this.rangeInput = document.getElementById('layout-input')
         this.rangeInput.oninput = () => this.changeHueValue()
+
+        this.load()
     }
 
     changeHueValue() {
         document.documentElement.style.cssText = `--hue: ${this.rangeInput.value}`
+        this.save()
+    }
+
+    load() {
+        this.rangeInput.value = JSON.parse(localStorage.getItem('@layout-range:')) || []
+        document.documentElement.style.cssText = `--hue: ${this.rangeInput.value}`
+    }
+
+    save() {
+        localStorage.setItem('@layout-range:', this.rangeInput.value)
     }
 }
